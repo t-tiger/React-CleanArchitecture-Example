919000235c190b369c0045eb7bf75192357bb3be
keijisan <tachiba@gmail.com>
2019-08-29 00:17:48 +0900

    add test for articleGateway, articleInteractor

---
+++ gateway/__tests__/articleGateway.test.ts
@@ -0,0 +1,45 @@
+import ArticleGateway from "../articleGateway";
+import ArticleDriver from "../../driver/articleDriver";
+import { Article } from "../../domain/article";
+import { Author } from "../../domain/author";
+import { ApiArticles } from "../../driver/entity/article";
+
+const articleDriver: ArticleDriver = {
+  findAll: (): Promise<ApiArticles> => {
+    throw "not implemented";
+  }
+};
+
+describe("#findAll", () => {
+  test("domain articles are returned", async () => {
+    const articles: ApiArticles = {
+      articles: [
+        {
+          id: 1,
+          name: "articleName",
+          author: {
+            id: 2,
+            name: "authorName"
+          },
+          createdAt: "2019-01-01T00:00:00.000Z"
+        }
+      ]
+    };
+    const findAllSpy = jest
+      .spyOn(articleDriver, "findAll")
+      .mockReturnValue(new Promise(resolve => resolve(articles)));
+    const articleGateway = new ArticleGateway(articleDriver);
+
+    expect(await articleGateway.findAll()).toEqual([
+      new Article(
+        1,
+        "articleName",
+        new Author(2, "authorName"),
+        new Date("2019-01-01")
+      )
+    ]);
+    expect(findAllSpy).toHaveBeenCalledTimes(1);
+    findAllSpy.mockClear();
+    findAllSpy.mockReset();
+  });
+});
---
+++ interactor/__tests__/articleInteractor.test.ts
@@ -0,0 +1,28 @@
+import { Article } from "../../domain/article";
+import ArticlePort from "../../interface/port/articlePort";
+import ArticleInteractor from "../articleInteractor";
+
+const articleGateway: ArticlePort = {
+  findAll: (): Promise<Article[]> => {
+    throw "not implemented";
+  }
+};
+
+describe("#fetchArticles", () => {
+  test("domain articles are returned", async () => {
+    const article1 = { id: 1 } as Article;
+    const article2 = { id: 2 } as Article;
+    const findAllSpy = jest
+      .spyOn(articleGateway, "findAll")
+      .mockReturnValue(new Promise(resolve => resolve([article1, article2])));
+    const articleInteractor = new ArticleInteractor(articleGateway);
+
+    expect(await articleInteractor.fetchArticles()).toEqual([
+      article1,
+      article2
+    ]);
+    expect(findAllSpy).toHaveBeenCalledTimes(1);
+    findAllSpy.mockClear();
+    findAllSpy.mockReset();
+  });
+});
