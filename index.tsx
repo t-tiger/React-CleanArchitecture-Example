8d22e14c66982ddbd6e806b93902f3fc63e6e800
keijisan <tachiba@gmail.com>
2019-08-28 23:12:50 +0900

    modify variable name

--- index.tsx
+++ index.tsx
@@ -6,5 +6,5 @@ import ArticleGateway from "./gateway/articleGateway";
 import ArticleDriver from "./driver/articleDriver";
 
 const gateway = new ArticleGateway(new ArticleDriver());
-const usecase = new ArticleInteractor(gateway);
-ReactDOM.render(<App useCase={usecase} />, document.getElementById("app"));
+const useCase = new ArticleInteractor(gateway);
+ReactDOM.render(<App useCase={useCase} />, document.getElementById("app"));
