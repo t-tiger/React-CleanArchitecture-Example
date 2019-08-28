41e9302e066f13abf1226844cf17be8e9a0fa77c
keijisan <tachiba@gmail.com>
2019-08-28 23:03:28 +0900

    add fetchArticle method

--- useCase/articleUseCase.ts
+++ useCase/articleUseCase.ts
@@ -1,9 +1,14 @@
 import ArticlePort from "../port/articlePort";
+import { Article } from "../domain/article";
 
 export default class ArticleUseCase {
   readonly articlePort: ArticlePort;
 
   constructor(articlePort: ArticlePort) {
     this.articlePort = articlePort;
+  }
+
+  async fetchArticles(): Promise<Article[]> {
+    return await this.articlePort.findAll();
   }
 }
