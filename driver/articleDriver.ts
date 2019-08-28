d0ef9172ed4b02e9f620e8d78b1adeac78d068b2
keijisan <tachiba@gmail.com>
2019-08-28 23:12:25 +0900

    remove TODO

--- driver/articleDriver.ts
+++ driver/articleDriver.ts
@@ -2,7 +2,6 @@ import { ApiArticles } from "./entity/article";
 
 export default class ArticleDriver {
   async findAll(): Promise<ApiArticles> {
-    // TODO baseURL using environment file
     const res = await fetch("http://localhost:3000/articles");
     return await res.json();
   }
