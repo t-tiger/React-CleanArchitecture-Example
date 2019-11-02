81fcbc24125feda25267ba77caa2295375a5b3c0
keijisan <tachiba@gmail.com>
2019-11-02 15:14:11 +0900

    remove spy

--- useCase/__tests__/articleUseCase.test.ts
+++ useCase/__tests__/articleUseCase.test.ts
@@ -2,24 +2,21 @@ import { Article } from "../../domain/article";
 import ArticleRepository from "../../interface/repository/articleRepository";
 import ArticleUseCaseImpl from "../articleUseCase";
 
-const articleRepository: ArticleRepository = {
-  findAll: (): Promise<Article[]> => {
+class ArticleRepositoryMock implements ArticleRepository {
+  findAll(): Promise<Article[]> {
     throw "not implemented";
   }
-};
+}
 
 describe("#fetchArticles", () => {
   test("domain articles are returned", async () => {
     const article1 = { id: 1 } as Article;
     const article2 = { id: 2 } as Article;
-    const findAllSpy = jest
-      .spyOn(articleRepository, "findAll")
-      .mockReturnValue(new Promise(resolve => resolve([article1, article2])));
-    const articleUseCase = new ArticleUseCaseImpl(articleRepository);
 
+    const mock = new ArticleRepositoryMock();
+    mock.findAll = async () => [article1, article2];
+
+    const articleUseCase = new ArticleUseCaseImpl(mock);
     expect(await articleUseCase.fetchArticles()).toEqual([article1, article2]);
-    expect(findAllSpy).toHaveBeenCalledTimes(1);
-    findAllSpy.mockClear();
-    findAllSpy.mockReset();
   });
 });
